17ee53c9827be8e1ce140bd369b6cfc5d98b475f
hwadong119 <jeonghwayun0119@naver.com>
2023-09-11 20:40:31 +0900

    Compiled Typescript

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,6 +1,15 @@
 import { Store } from "../core/com";
 
-export default new Store({
+interface State {
+  photo: string
+  name: string
+  email: string
+  blog: string
+  github: string
+  repository: string
+}
+
+export default new Store<State>({
   photo: 'https://velog.velcdn.com/images/hwadong119/post/90ae1393-c882-4dd8-8b7e-8ea38386cd08/image.png',
   name: 'HWADONG',
   email: 'jeonghwayun0119@naver.com',
